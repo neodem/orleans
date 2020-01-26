351a5858b9436965bb1b0bd5b0274b8162a65dc1
Vincent Fumo <neodem@gmail.com>
2020-01-26 17:23:02 -0500

    * adds swagger  (incomplete)

--- src/main/java/com/neodem/orleans/web/GameController.java
+++ src/main/java/com/neodem/orleans/web/GameController.java
@@ -13,9 +13,9 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +29,7 @@ import java.util.UUID;
  * Created by Vincent Fumo (neodem@gmail.com)
  * Created on 12/27/19
  */
-@Tag(name = "gameController", description = "the Game Engine API")
+//@Tag(name = "gameController", description = "the Game Engine API")
 @RestController
 @RequestMapping("/game")
 public class GameController {
@@ -37,12 +37,14 @@ public class GameController {
     @Resource
     private GameMaster gameMaster;
 
-    @Operation(summary = "Initialize Game", description = "Init a new game", tags = { "gameController" })
+    @Operation(summary = "Initialize Game", description = "Init a new game")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(array = @ArraySchema(schema = @Schema(implementation = GameState.class))))
     })
-    @RequestMapping("/init")
-    public GameState gameInit(@Parameter(description="Comma Seperated list of player names for the game (2-4) allowed")  @RequestParam(value = "playerNames") List<String> names) {
+    @RequestMapping(value = "/init", method = RequestMethod.POST)
+    public GameState gameInit(
+            @Parameter(description = "Comma Seperated list of player names for the game (2-4) allowed") @RequestParam(value = "playerNames") List<String> names
+    ) {
         UUID uuid = UUID.randomUUID();
         String gameId = uuid.toString();
 
@@ -50,48 +52,70 @@ public class GameController {
         return gameState;
     }
 
-    @RequestMapping("/{gameId}/startGame")
-    public GameState startGame(@PathVariable(value = "gameId") String gameId) {
+    @Operation(summary = "Start a Game", description = "Start a game that has been previously initialized")
+    @RequestMapping(value = "/{gameId}/startGame", method = RequestMethod.POST)
+    public GameState startGame(
+            @Parameter(description = "the gameId") @PathVariable(value = "gameId") String gameId
+    ) {
         GameState gameState = gameMaster.startGame(gameId);
         return gameState;
     }
 
-    @RequestMapping("/{gameId}/{playerId}/plan")
+    @Operation(summary = "Submit a plan for a player", description = "On their turn a player may submit as many plans as they can 'afford' each call moves one follower from their market to the desired action spot")
+    @RequestMapping(value = "/{gameId}/{playerId}/plan", method = RequestMethod.POST)
     public GameState submitPlan(
-            @PathVariable(value = "gameId") String gameId,
-            @PathVariable(value = "playerId") String playerId,
-            @RequestParam(value = "action") String action,
-            @RequestParam(value = "marketSlot") int marketSlot,
-            @RequestParam(value = "actionSlot") int actionSlot
+            @Parameter(description = "the gameId") @PathVariable(value = "gameId") String gameId,
+            @Parameter(description = "the playerId") @PathVariable(value = "playerId") String playerId,
+            @Parameter(description = "the action to move the follower to") @RequestParam(value = "action") String action,
+            @Parameter(description = "the market slot to move the follower from (0 based)") @RequestParam(value = "marketSlot") int marketSlot,
+            @Parameter(description = "the action slot to move the follower to (0 based)") @RequestParam(value = "actionSlot") int actionSlot
     ) {
         GameState gameState = gameMaster.addToPlan(gameId, playerId, ActionType.valueOf(action), marketSlot, actionSlot);
         return gameState;
     }
 
-    @RequestMapping("/{gameId}/{playerId}/action")
-    public GameState doAction(@PathVariable(value = "gameId") String gameId, @PathVariable(value = "playerId") String playerId, @RequestParam(value = "action") String action, @RequestParam Map<String, String> allParams) {
+    @Operation(summary = "execute an action", description = "When allowed, a player may execte an action")
+    @RequestMapping(value = "/{gameId}/{playerId}/action", method = RequestMethod.POST)
+    public GameState doAction(
+            @Parameter(description = "the gameId") @PathVariable(value = "gameId") String gameId,
+            @Parameter(description = "the playerId") @PathVariable(value = "playerId") String playerId,
+            @Parameter(description = "the action to execute") @RequestParam(value = "action") String action,
+            @Parameter(description = "some actions require extra params to be added for context") @RequestParam Map<String, String> allParams
+    ) {
         Map<AdditionalDataType, String> additionalDataMap = convertAdditionalDataMap(allParams, "action");
         GameState gameState = gameMaster.doAction(gameId, playerId, ActionType.valueOf(action), additionalDataMap);
         return gameState;
     }
 
-    @RequestMapping("/{gameId}/{playerId}/pass")
-    public GameState passActionPhase(@PathVariable(value = "gameId") String gameId, @PathVariable(value = "playerId") String playerId) {
+    @Operation(summary = "player pass", description = "When done submitting plans/torture, the player can and should pass")
+    @RequestMapping(value = "/{gameId}/{playerId}/pass", method = RequestMethod.POST)
+    public GameState passActionPhase(
+            @Parameter(description = "the gameId") @PathVariable(value = "gameId") String gameId,
+            @Parameter(description = "the playerId") @PathVariable(value = "playerId") String playerId
+    ) {
         GameState gameState = gameMaster.pass(gameId, playerId);
         return gameState;
     }
 
-    // player needs to submit one per coin owed.. game will not proceed until all coins are accounted for
-    @RequestMapping("/{gameId}/{playerId}/torture")
-    public GameState torturePlan(@PathVariable(value = "gameId") String gameId, @PathVariable(value = "playerId") String playerId, @RequestParam(value = "tortureType") String tortureTypeString, @RequestParam Map<String, String> allParams) {
+    @Operation(summary = "Submit a torture for a player", description = "When a player owes coins, they must submit one torture plan per coin owed")
+    @RequestMapping(value = "/{gameId}/{playerId}/torture", method = RequestMethod.POST)
+    public GameState torturePlan(
+            @Parameter(description = "the gameId") @PathVariable(value = "gameId") String gameId,
+            @Parameter(description = "the playerId") @PathVariable(value = "playerId") String playerId,
+            @Parameter(description = "the type of torture to endure") @RequestParam(value = "tortureType") String tortureTypeString,
+            @Parameter(description = "some tortures require extra params to be added for context") @RequestParam Map<String, String> allParams
+    ) {
         TortureType tortureType = TortureType.valueOf(tortureTypeString);
         Map<AdditionalDataType, String> additionalDataMap = convertAdditionalDataMap(allParams, "tortureType");
         GameState gameState = gameMaster.torturePlan(gameId, playerId, tortureType, additionalDataMap);
         return gameState;
     }
 
-    @RequestMapping("/{gameId}/gameState")
-    public GameState gameState(@PathVariable(value = "gameId") String gameId) {
+    @Operation(summary = "get current game state", description = "This will return the game state at this moment in time")
+    @RequestMapping(value = "/{gameId}/gameState", method = RequestMethod.POST)
+    public GameState gameState(
+            @Parameter(description = "the gameId") @PathVariable(value = "gameId") String gameId
+    ) {
         GameState gameState = gameMaster.getGameState(gameId);
         return gameState;
     }
